3e325b23d138dfbd63eef8c40d779fff3bf98616
raluka <badoi.raluca@gmail.com>
2015-07-24 22:01:15 +0200

    made single quotes from double qoutes to make rubocop happy

--- db/migrate/20150724182915_add_subject_and_comment_template_for_email_settings.rb
+++ db/migrate/20150724182915_add_subject_and_comment_template_for_email_settings.rb
@@ -2,8 +2,10 @@ class AddSubjectAndCommentTemplateForEmailSettings < ActiveRecord::Migration
   def change
     add_column :email_settings, :comment_subject, :string
     add_column :email_settings, :comment_template, :text
-    comment_subject = "New comment has been posted for {eventtitle}"
-    comment_template_text = "Dear {name},\r\n\r\nUser {comment_user} posted a new comment for event {eventtitle} of {conference} conference.\r\n\r\n{comment_body}\r\n\r\nTo reply to this comment, please go to {comment_reply}\r\n\r\nBest wishes\r\n\r\n{conference} Team"
+    comment_subject = 'New comment has been posted for {eventtitle}'
+    comment_template_text = 'Dear {name},\r\n\r\nUser {comment_user} posted a new comment for event {eventtitle}
+                             of {conference} conference.\r\n\r\n{comment_body}\r\n\r\nTo reply to this comment, please
+                             go to {comment_reply}\r\n\r\nBest wishes\r\n\r\n{conference} Team'
     EmailSettings.all.each do |email_setting|
       email_setting.comment_subject = comment_subject
       email_setting.comment_template = comment_template_text
