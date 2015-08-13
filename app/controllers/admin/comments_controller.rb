74e03b28b4a78392ca4deab54f51af9ec70bdd52
raluka <badoi.raluca@gmail.com>
2015-08-13 16:24:33 +0200

    refactored index in comment_controller

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -4,14 +4,16 @@ module Admin
 
     def index
       @comments = Comment.all.order(created_at: :desc)
+      @unread_comments = unread
+      @posted_comments = posted
     end
 
-    def unread_comments
-      @unread_comments = Comment.where(created_at: (current_user.last_sign_in_at..Time.now)).order(:commentable_id, created_at: :desc)
+    def unread
+       Comment.where(created_at: (current_user.last_sign_in_at..Time.now)).order(created_at: :desc)
     end
 
-    def posted_comments
-      @posted_comments = Comment.where(user_id: current_user.id).order(created_at: :desc)
+    def posted
+       Comment.where(user_id: current_user.id).order(created_at: :desc)
     end
   end
 end
