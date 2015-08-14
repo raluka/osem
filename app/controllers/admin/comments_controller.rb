19592acc97fb3c648888bd4e91bff9c4d32ef345
raluka <badoi.raluca@gmail.com>
2015-08-14 18:20:50 +0200

    added some modifications at navigation button

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -3,17 +3,19 @@ module Admin
     load_and_authorize_resource
 
     def index
-      @comments = Comment.all.order(created_at: :desc)
+      @comments = Comment.all.order(:commentable_id, created_at: :desc)
       @unread_comments = unread
       @posted_comments = posted
     end
 
+    private
+
     def unread
-       Comment.where(created_at: (current_user.last_sign_in_at..Time.now)).order(created_at: :desc)
+       Comment.where(created_at: (current_user.last_sign_in_at..Time.now)).order(:commentable_id, created_at: :desc)
     end
 
     def posted
-       Comment.where(user_id: current_user.id).order(created_at: :desc)
+      Comment.where(user_id: current_user.id).order(:commentable_id, created_at: :desc)
     end
   end
 end
